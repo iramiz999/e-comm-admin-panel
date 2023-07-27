b238bedf223815fda530900256c7c3e8d60d1b39
Ramiz Zarzor <ramizzarzor1998@gmail.com>
2023-07-27 17:28:22 +0300

    overUpdated

--- app/api/[storeId]/products/route.ts
+++ app/api/[storeId]/products/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { auth } from '@clerk/nextjs';
 
 import prismadb from '@/lib/prismadb';
- 
+
 export async function POST(
   req: Request,
   { params }: { params: { storeId: string } }
@@ -12,19 +12,7 @@ export async function POST(
 
     const body = await req.json();
 
-    const {
-      name,
-      price,
-      categoryId,
-      colorId,
-      sizeId,
-      images,
-      isFeatured,
-      isArchived,
-
-
-
-     } = body;
+    const { name, price, categoryId, colorId, sizeId, images, isFeatured, isArchived } = body;
 
     if (!userId) {
       return new NextResponse("Unauthenticated", { status: 403 });
@@ -34,12 +22,10 @@ export async function POST(
       return new NextResponse("Name is required", { status: 400 });
     }
 
-     if (!images || !images.length)  {
-      return new NextResponse("Images are required", {status:400})
-
+    if (!images || !images.length) {
+      return new NextResponse("Images are required", { status: 400 });
     }
 
-    
     if (!price) {
       return new NextResponse("Price is required", { status: 400 });
     }
@@ -48,14 +34,13 @@ export async function POST(
       return new NextResponse("Category id is required", { status: 400 });
     }
 
-    if (!sizeId) {
-      return new NextResponse("Size id is required", { status: 400 });
-    }
-
     if (!colorId) {
       return new NextResponse("Color id is required", { status: 400 });
     }
 
+    if (!sizeId) {
+      return new NextResponse("Size id is required", { status: 400 });
+    }
 
     if (!params.storeId) {
       return new NextResponse("Store id is required", { status: 400 });
@@ -64,7 +49,7 @@ export async function POST(
     const storeByUserId = await prismadb.store.findFirst({
       where: {
         id: params.storeId,
-        userId,
+        userId
       }
     });
 
@@ -81,39 +66,34 @@ export async function POST(
         categoryId,
         colorId,
         sizeId,
-
-
         storeId: params.storeId,
-        images:{
-          createMany:{
-            data:[
-              ...images.map((image:{url:string}) => image)
-            ]
-          }
-        }
-      }
+        images: {
+          createMany: {
+            data: [
+              ...images.map((image: { url: string }) => image),
+            ],
+          },
+        },
+      },
     });
   
     return NextResponse.json(product);
   } catch (error) {
-    console.log('[PRODUCT_POST]', error);
+    console.log('[PRODUCTS_POST]', error);
     return new NextResponse("Internal error", { status: 500 });
   }
 };
 
 export async function GET(
   req: Request,
-  { params }: { params: { storeId: string } }
+  { params }: { params: { storeId: string } },
 ) {
   try {
-
-    const { searchParams} = new URL(req.url)
-    const categoryId = searchParams.get("categeoryId") || undefined
-    const colorId = searchParams.get("colorId") || undefined
-    const sizeId = searchParams.get("sizeId") || undefined
-    const isFeatured = searchParams.get("isFeatured")
-
-
+    const { searchParams } = new URL(req.url)
+    const categoryId = searchParams.get('categoryId') || undefined;
+    const colorId = searchParams.get('colorId') || undefined;
+    const sizeId = searchParams.get('sizeId') || undefined;
+    const isFeatured = searchParams.get('isFeatured');
 
     if (!params.storeId) {
       return new NextResponse("Store id is required", { status: 400 });
@@ -125,20 +105,17 @@ export async function GET(
         categoryId,
         colorId,
         sizeId,
-        isFeatured:isFeatured? true : undefined,
-        isArchived : false
-
+        isFeatured: isFeatured ? true : undefined,
+        isArchived: false,
       },
-      include:{
-        images:true,
-        category:true,
-        color:true,
-        size:true
-
+      include: {
+        images: true,
+        category: true,
+        color: true,
+        size: true,
       },
-
-      orderBy:{
-        createdAt:'desc'
+      orderBy: {
+        createdAt: 'desc',
       }
     });
   
